e78ebabd2c350fa88a85e0ff4c6dfb4d8c30f240
Victor Liu <vliu00@stuy.edu>
2019-03-12 18:28:45 -0400

    added the selecting median of 3 values for pivot index feature

--- Quick.java
+++ Quick.java
@@ -10,7 +10,7 @@ public class Quick{
     }
 
     private static int quickHelp(int[] data, int k, int start, int end){
-        int pivotIndex = partition(data, start, end);
+        int pivotIndex = partitionDutch(data, start, end);
         if (k < pivotIndex){
             return quickHelp(data,k,start,pivotIndex-1);
         }
@@ -21,7 +21,15 @@ public class Quick{
         }
     }
 
-    private int partitionDutch(int[] data,int start, int end){
+    /*Modify the array such that:
+    *1. Only the indices from start to end inclusive are considered in range
+    *2. A random index from start to end inclusive is chosen, the corresponding
+    *   element is designated the pivot element.
+    *3. all elements in range that are smaller than the pivot element are placed before the pivot element.
+    *4. all elements in range that are larger than the pivot element are placed after the pivot element.
+    *@return the index of the final position of the pivot element.
+    */
+    private static int partitionDutch(int[] data,int start, int end){
       int inputtedEnd = end;
       int inputtedStart = start;
       int pivot = 0; //to initialize
@@ -29,19 +37,18 @@ public class Quick{
       Random r = new Random();
       for (int i = 0; i < inputtedEnd - inputtedStart; i++){
           if (i == 0){
-              int randValue1 = Math.abs(r.nextInt(end - start)) + start;
-              int randValue2 = Math.abs(r.nextInt(end - start)) + start;
-              int randValue3 = Math.abs(r.nextInt(end - start)) + start;
-              int midValue = 0;
-              if (randValue1 < randValue2 && randValue2 < randValue3){
-                midValue = randValue2;
-              if (randValue2 < randValue3 && randValue3 < randValue1){
-                midValue = randValue3;
+              //finding the median value of data[start], data[start], and data[randomly chosen index in between]
+              int startVal = data[start];
+              int endVal = data[end];
+              int randIndex = Math.abs(r.nextInt(end - start)) + start;
+              int randVal = data[randIndex];
+              if (startVal <= randVal && randVal <= endVal || startVal >= randVal && randVal >= endVal){
+                pivotIndex = randIndex;
+              } else if (randVal <= endVal && endVal <= startVal || randVal >= endVal && endVal >= startVal){
+                pivotIndex = end;
+              } else if (endVal <= startVal && startVal <= randVal || endVal >= startVal && startVal >= randVal){
+                pivotIndex = start;
               }
-              if (randValue3 < randValue1 && randValue1 < randValue2){
-                midValue = randValue1;
-              }
-              pivotIndex = ;
               pivot = data[pivotIndex];
               int a = data[start];
               data[start] = data[pivotIndex];
@@ -72,54 +79,5 @@ public class Quick{
           }
       }
       return pivotIndex;
-    }
-
-    /*Modify the array such that:
-    *1. Only the indices from start to end inclusive are considered in range
-    *2. A random index from start to end inclusive is chosen, the corresponding
-    *   element is designated the pivot element.
-    *3. all elements in range that are smaller than the pivot element are placed before the pivot element.
-    *4. all elements in range that are larger than the pivot element are placed after the pivot element.
-    *@return the index of the final position of the pivot element.
-    */
-    public static int partition(int [] data, int start, int end){
-        int inputtedEnd = end;
-        int inputtedStart = start;
-        int pivot = 0; //to initialize
-        int pivotIndex = start;
-        Random r = new Random();
-        for (int i = 0; i < inputtedEnd - inputtedStart; i++){
-            if (i == 0){
-                pivotIndex = Math.abs(r.nextInt(end - start)) + start;
-                pivot = data[pivotIndex];
-                int a = data[start];
-                data[start] = data[pivotIndex];
-                data[pivotIndex] = a;
-                pivotIndex = start;
-                start++;
-            }
-            if (start == end){
-                if (data[start] >= pivot){
-                    int c = data[start-1];
-                    data[start-1] = data[pivotIndex];
-                    data[pivotIndex] = c;
-                    pivotIndex = start - 1;
-                } else {
-                    int c = data[start];
-                    data[start] = data[pivotIndex];
-                    data[pivotIndex] = c;
-                    pivotIndex = start;
-                }
-            }
-            if (data[start] < pivot){
-                start++;
-            } else {
-                int b = data[end];
-                data[end] = data[start];
-                data[start] = b;
-                end--;
-            }
-        }
-        return pivotIndex;
     }
 }
