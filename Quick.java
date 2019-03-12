dcd8d43fb9f4f89d48530cb5c3925a3b17539b4f
vliu00 <vliu00@stuy.edu>
2019-03-12 14:51:34 -0400

    working on easy way to handle duplicates

--- Quick.java
+++ Quick.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class Quick{
 
-    /*return the value that is the kth smallest value of the array. 
+    /*return the value that is the kth smallest value of the array.
     */
     public static int quickselect(int[] data, int k){
         return quickHelp(data, k, 0, data.length-1);
@@ -21,11 +21,63 @@ public class Quick{
         }
     }
 
+    private int partitionDutch(int[] data,int start, int end){
+      int inputtedEnd = end;
+      int inputtedStart = start;
+      int pivot = 0; //to initialize
+      int pivotIndex = start;
+      Random r = new Random();
+      for (int i = 0; i < inputtedEnd - inputtedStart; i++){
+          if (i == 0){
+              int randValue1 = Math.abs(r.nextInt(end - start)) + start;
+              int randValue2 = Math.abs(r.nextInt(end - start)) + start;
+              int randValue3 = Math.abs(r.nextInt(end - start)) + start;
+              int midValue = 0;
+              if (randValue1 < randValue2 && randValue2 < randValue3){
+                midValue = randValue2;
+              if (randValue2 < randValue3 && randValue3 < randValue1){
+                midValue = randValue3;
+              }
+              if (randValue3 < randValue1 && randValue1 < randValue2){
+                midValue = randValue1;
+              }
+              pivotIndex = ;
+              pivot = data[pivotIndex];
+              int a = data[start];
+              data[start] = data[pivotIndex];
+              data[pivotIndex] = a;
+              pivotIndex = start;
+              start++;
+          }
+          if (start == end){
+              if (data[start] >= pivot){
+                  int c = data[start-1];
+                  data[start-1] = data[pivotIndex];
+                  data[pivotIndex] = c;
+                  pivotIndex = start - 1;
+              } else {
+                  int c = data[start];
+                  data[start] = data[pivotIndex];
+                  data[pivotIndex] = c;
+                  pivotIndex = start;
+              }
+          }
+          if (data[start] < pivot){
+              start++;
+          } else {
+              int b = data[end];
+              data[end] = data[start];
+              data[start] = b;
+              end--;
+          }
+      }
+      return pivotIndex;
+    }
 
     /*Modify the array such that:
     *1. Only the indices from start to end inclusive are considered in range
-    *2. A random index from start to end inclusive is chosen, the corresponding 
-    *   element is designated the pivot element. 
+    *2. A random index from start to end inclusive is chosen, the corresponding
+    *   element is designated the pivot element.
     *3. all elements in range that are smaller than the pivot element are placed before the pivot element.
     *4. all elements in range that are larger than the pivot element are placed after the pivot element.
     *@return the index of the final position of the pivot element.
