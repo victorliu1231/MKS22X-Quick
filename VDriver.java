8dd5773519c92431227729bbb91eab0a5ce5e5bf
Victor Liu <vliu00@stuy.edu>
2019-03-12 19:46:31 -0400

    adding a runtime test driver

--- VDriver.java
+++ VDriver.java
@@ -1,27 +1,35 @@
 import java.util.Random;
+import java.util.Arrays;
 
 public class VDriver{
     public static void main(String[]args){
-        /*
+
+        //TEST RUNTIME
+        
         int[]ary = { 2, 10, 15, 23, 0,  5};  //sorted :  {0,2,5,10,15,23}
-        System.out.println(Quick.quickselect( ary , 0 )); //would return 0
+        /*System.out.println(Quick.quickselect( ary , 0 )); //would return 0
         System.out.println(Quick.quickselect( ary , 1 ));  //would return 2
         System.out.println(Quick.quickselect( ary , 2 ));  //would return 5
         System.out.println(Quick.quickselect( ary , 3 ));  //would return 10
         System.out.println(Quick.quickselect( ary , 4 )); // would return 15
-        System.out.println(Quick.quickselect( ary , 5 ));  //would return 23
-        */
+        System.out.println(Quick.quickselect( ary , 5 ));  //would return 23*/
+        Quick.quicksort(ary);
+        System.out.println(Arrays.toString(ary));
+        
 
         for (int i = 1; i < 1000; i++) {
             try {
                 Random gen = new Random();
                 int[] test = new int[i];
+                int[] duplicate = new int[i];
                 for (int j = 0; j < test.length; j++) {
-                    test[j] = gen.nextInt();
+                    int randGenInt = gen.nextInt() % 1000;
+                    test[j] = randGenInt;
+                    duplicate[j] = randGenInt;
                 }
-                int k = Math.abs(gen.nextInt()) % test.length;
-                int kthElement = Quick.quickselect(test, k);
-                if (!test(test, k, kthElement)) {
+                Quick.quicksort(test);
+                if (!hasSortedRight(test, duplicate)) {
+                    //System.out.println("test: "+Arrays.toString(test));
                     System.out.print("FAILURE ON LENGTH " + i);
                     System.exit(1);
                 }
@@ -30,21 +38,71 @@ public class VDriver{
                 System.out.print("FAILURE ON LENGTH " + i);
                 System.exit(1);
             }
+            //System.out.println("--------");
         }
         System.out.println("R: SUCCESS"); //R for random
 
+        
         Random why = new Random();
         int[] ugh = new int[10000];
+        int[] duplicate1 = new int[10000];
         for (int i = 0; i < ugh.length; i++){
-            ugh[i] = Math.abs(why.nextInt(2));
+            int randGenInt = Math.abs(why.nextInt(2));
+            ugh[i] = randGenInt;
+            duplicate1[i] = randGenInt;
         }
-        int kthElement = Quick.quickselect(ugh,100);
-        System.out.println(kthElement);
-        System.out.println(test(ugh, 100, kthElement));
+        Quick.quicksort(ugh);
+        System.out.println(hasSortedRight(ugh, duplicate1));
 
-        kthElement = Quick.quickselect(ugh,7000);
-        System.out.println(kthElement);
-        System.out.println(test(ugh, 7000, kthElement));
+        Random why2 = new Random();
+        int[] ugh2 = new int[10000];
+        int[] duplicate2 = new int[10000];
+        for (int i = 0; i < ugh2.length; i++){
+            int randGenInt = Math.abs(why2.nextInt(1));
+            ugh2[i] = randGenInt;
+            duplicate2[i] = randGenInt;
+        }
+        Quick.quicksort(ugh2);
+        System.out.println(hasSortedRight(ugh2, duplicate2));
+
+        for (int i = 1; i < 1000; i++) {
+            try {
+                Random gen = new Random();
+                int[] test = new int[i];
+                int[] duplicate = new int[i];
+                for (int j = 0; j < test.length; j++) {
+                    int randGenInt = gen.nextInt() % 1000;
+                    test[j] = randGenInt;
+                    duplicate[j] = randGenInt;
+                }
+                long quickStartMillis = System.currentTimeMillis();
+                Quick.quicksort(test);
+                long quickEndMillis = System.currentTimeMillis();
+                long quickDiffMillis = quickEndMillis - quickStartMillis;
+                long selectionStartMillis = System.currentTimeMillis();
+                selectionSort(duplicate);
+                long selectionEndMillis = System.currentTimeMillis();
+                long selectionDiffMillis = selectionEndMillis - selectionStartMillis;
+                if (selectionDiffMillis < quickDiffMillis){
+                    System.out.println("selection is "+((quickDiffMillis - selectionDiffMillis))+" milliseconds faster");
+                    System.out.println("SLOWER ON LENGTH " + i);
+                    System.out.println("--------------------------------");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.print("FAILURE ON LENGTH " + i);
+            }
+        }
+    }
+
+    public static boolean hasSortedRight(int[] test, int[] duplicate){
+        selectionSort(duplicate);
+        for (int i = 0; i < test.length; i++){
+            if (test[i] != duplicate[i]){
+                return false;
+            }
+        }
+        return true;
     }
 
     public static boolean test(int[] data, int k, int kthElement){
@@ -61,5 +119,27 @@ public class VDriver{
             }
         }
         return true;
+    }
+
+    public static void selectionSort(int [] ary) { //O(n^2); n passes with n work each
+        int min;
+        int indexOfMin = -1;
+        int storer;
+        for (int i = 0; i < ary.length-1; i++){
+            min = ary[i];
+            indexOfMin = i;
+            //for loop has "i+1" so that the "first value" being iterated through doesn't
+            //include the already established values to be minimums
+            for (int n = i+1; n < ary.length; n++){
+                if (ary[n] < min){
+                    min = ary[n];
+                    indexOfMin = n;
+                }
+            }
+            //swaps the minimum and the "first" value
+            storer = ary[i];
+            ary[i] = min;
+            ary[indexOfMin] = storer;
+        }
     }
 }
